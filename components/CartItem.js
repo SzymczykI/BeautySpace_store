520e0495014251a841699e5a654f5c7e20876ea3
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-08-25 18:08:46 +0200

    deployment

---
+++ components/CartItem.js
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import { decrease, increase } from '../store/Actions'
+
+const CartItem = ({ item, dispatch, cart }) => {
+    return (
+        <tr>
+            <td style={{ width: '100px', overflow: 'hidden' }}>
+                <img src={item.images[0].url} alt={item.images[0].url}
+                    className="img-thumbnail w-100"
+                    style={{ minWidth: '80px', height: '80px' }} />
+            </td>
+
+            <td style={{ minWidth: '200px' }} className="align-middle">
+                <h5 className="text-capitalize">
+                    <Link href={`/product/${item._id}`}>
+                        <a>{item.title}</a>
+                    </Link>
+                </h5>
+                <h6>{item.description}</h6>
+                <h6>${item.quantity * item.price}</h6>
+            </td>
+
+            <td className="align-middle" style={{ minWidth: '150px' }}>
+                <button onClick={() => dispatch(decrease(cart, item._id))}
+                    className="btn btn-outline-secondary"
+                    disabled={item.quantity === 1 ? true : false} > - </button>
+                <span className="px-3">{item.quantity}</span>
+                <button onClick={() => dispatch(increase(cart, item._id))}
+                    className="btn btn-outline-secondary"
+                    disabled={item.quantity === item.inStock ? true : false} > + </button>
+            </td>
+
+            <td className="align-middle" style={{ minWidth: '50px', cursor: 'pointer' }}>
+                <i className="far fa-trash-alt" aria-hidden="true"></i>
+            </td>
+        </tr>
+    )
+}
+
+export default CartItem
--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,15 @@
-import Document, {Html, Head, Main, NextScript} from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-    render(){
-        return(
+    render() {
+        return (
             <Html lang="en">
                 <Head>
                     <meta name="description" content="E-commerce beauty website" />
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"></link>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"></link>
                     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"></script>
                     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
                 </Head>
                 <body>
